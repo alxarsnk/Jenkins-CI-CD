cf3424c35c0e490c5da943e07d4ac42aa90ccb95
Alexandr Arsenyuk <arsenyukofficial@gmail.com>
2020-01-21 22:37:36 +0300

    Second commit

--- ExamWork/ExamWorkTests/ExamWorkTests.swift
+++ ExamWork/ExamWorkTests/ExamWorkTests.swift
@@ -11,24 +11,8 @@ import XCTest
 
 class ExamWorkTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_001() {
+        let variable = true
+        XCTAssert(variable, "Variable is true")
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
---
+++ ExamWork/Pods/Target Support Files/Pods-ExamWork/Pods-ExamWork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExamWork : NSObject
+@end
+@implementation PodsDummy_Pods_ExamWork
+@end
